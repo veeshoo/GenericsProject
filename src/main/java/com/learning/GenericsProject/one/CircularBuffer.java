e489d2d173da3e137a33b5b837f67ec716a66d73
Vishal Sehgal <veeshoo@gmail.com>
2017-08-15 15:41:00 +0530

    First Commit

---
+++ src/main/java/com/learning/GenericsProject/one/CircularBuffer.java
@@ -0,0 +1,35 @@
+package com.learning.GenericsProject.one;
+
+public class CircularBuffer {
+	
+	private final Object[] buffer;
+	private int readCursor = 0;
+	private int writeCursor = 0;
+
+	public CircularBuffer(int size) {
+		buffer = new Object[size];
+	}
+
+	public boolean offer(Object value) {
+		if(buffer[writeCursor]!=null) {
+			return false;
+		}
+		buffer[writeCursor] = value;
+		writeCursor = next(writeCursor);
+		return true;
+	}
+
+	private int next(int index) {
+		return (index + 1) % buffer.length;
+	}
+
+	public Object poll() {
+		Object value = buffer[readCursor];
+		if(value!=null) {
+			buffer[readCursor] = null;
+			readCursor = next(readCursor);
+		}
+		return value;
+	}
+
+}
